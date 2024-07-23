81a7ab61b03d82cc4d55a74e392ceb7af83c97a7
Brady <brady.wemette@digitalupgrade.net>
2024-07-22 19:16:14 -0500

    add a underconstruction page

--- construction.js
+++ construction.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.querySelector('.learn-btn');
+    const button = document.querySelector('.cta');
 
     button.addEventListener('click', () => {
 
-        window.location.href = "construction.html";
+        window.location.href = "index.html";
     });
 });
